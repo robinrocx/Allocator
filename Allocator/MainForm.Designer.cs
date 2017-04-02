f98f357f88caf4ae6cf90c39e93b5dcbec8091e1
Robin <robinraj.rajan43@gmail.com>
2017-04-02 15:09:53 +0530

    Added files for the first time

---
+++ Allocator/MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Allocator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.masterButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.masterTextBox = new System.Windows.Forms.TextBox();
+            this.generatorButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.generatorButton, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.46763F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.53238F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(424, 139);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.Controls.Add(this.masterButton, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1, 1);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 88F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(422, 89);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // masterButton
+            // 
+            this.masterButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.masterButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.masterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masterButton.Location = new System.Drawing.Point(307, 26);
+            this.masterButton.Name = "masterButton";
+            this.masterButton.Size = new System.Drawing.Size(101, 36);
+            this.masterButton.TabIndex = 2;
+            this.masterButton.Text = "Open Master";
+            this.masterButton.UseVisualStyleBackColor = false;
+            this.masterButton.Click += new System.EventHandler(this.masterButton_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.masterTextBox, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(1, 1);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.91139F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 67.08861F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(293, 87);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(96, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Task Workbook";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // masterTextBox
+            // 
+            this.masterTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.masterTextBox.Location = new System.Drawing.Point(10, 31);
+            this.masterTextBox.Margin = new System.Windows.Forms.Padding(0);
+            this.masterTextBox.Multiline = true;
+            this.masterTextBox.Name = "masterTextBox";
+            this.masterTextBox.ReadOnly = true;
+            this.masterTextBox.Size = new System.Drawing.Size(273, 53);
+            this.masterTextBox.TabIndex = 1;
+            this.masterTextBox.Text = "Open the Task Workbook containing the details of the tasks assigned..";
+            this.masterTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // generatorButton
+            // 
+            this.generatorButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.generatorButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.generatorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generatorButton.Location = new System.Drawing.Point(155, 96);
+            this.generatorButton.Name = "generatorButton";
+            this.generatorButton.Size = new System.Drawing.Size(113, 36);
+            this.generatorButton.TabIndex = 1;
+            this.generatorButton.Text = "Generate Excels";
+            this.generatorButton.UseVisualStyleBackColor = false;
+            this.generatorButton.Click += new System.EventHandler(this.generatorButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 139);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "Allocator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button masterButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button generatorButton;
+        private System.Windows.Forms.TextBox masterTextBox;
+    }
+}
+
---
+++ Allocator/MainForm.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using Excel=Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Drawing;
+using System.Collections.Generic;
+namespace Allocator
+{
+    public partial class MainForm : Form
+    {
+        Excel.Application xlApp = new Excel.Application();
+        Excel.Workbook workbook;
+        string masterWorkbook;
+        string masterText = "Open the Task Workbook containing the details of the tasks assigned..";
+        int namesIndex = 0;
+        int masterColCount = 0;
+        int formIdIndex = 0;
+        string filePath = null;
+        string masterSheetName;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void masterButton_Click(object sender, EventArgs e)
+        {
+            masterWorkbook = null;
+            filePath = null;
+            masterColCount = 0;
+            namesIndex = 0;
+            formIdIndex = 0;
+            OpenFileDialog opMaster = new OpenFileDialog();
+            opMaster.Multiselect = false;
+            opMaster.ShowDialog();
+            if (opMaster.FileNames.Length == 0)
+                MessageBox.Show("No Workbook was selected.\nPlease try again...");
+            else
+            {
+                masterTextBox.Text = "The selected workbook is being analysed. Please wait...";
+                if (opMaster.FileName.Substring(opMaster.FileName.Length - 5) != ".xlsx")
+                {
+                    MessageBox.Show("The selected Workbook is not an Excel Workbook!!! Please try again...");
+                    masterTextBox.Text = masterText;
+                }
+                else
+                {
+                    try
+                    {
+                        masterColCount = 0;
+                        Excel.Range range;
+                        workbook = xlApp.Workbooks.Open(opMaster.FileName);
+                        Excel.Worksheet worksheet = workbook.ActiveSheet;
+                        int col = 1;
+                        while (true)
+                        {
+                            range = worksheet.Cells[1, col];
+                            if (range.Value != null)
+                            {
+                                masterColCount++;
+                                col++;
+                                continue;
+                            }
+                            else
+                                break;
+                        }
+
+                        while ((namesIndex <= 0 || namesIndex > masterColCount) || (formIdIndex <= 0 || formIdIndex > masterColCount))
+                        {
+                            try
+                            {
+                                namesIndex = int.Parse(Showdialog());
+                            }
+                            catch (FormatException ex)
+                            {
+                                MessageBox.Show("Enter a valid Integer...");
+                            }
+                        }
+                        masterSheetName = worksheet.Name;
+                        masterTextBox.Text = masterWorkbook = opMaster.FileName;
+                        workbook.Close();
+                        xlApp.Quit();
+                        filePath = masterWorkbook.Substring(0, masterWorkbook.LastIndexOf("\\") + 1);
+                        MessageBox.Show("The workbook has been successfully added...");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("The selected Workbook cannot be opened!!!Please try again...");
+                        masterTextBox.Text = masterText;
+                        xlApp.Quit();
+                    }
+                }
+            }
+        }
+
+        private void generatorButton_Click(object sender, EventArgs e)
+        {
+            if (masterWorkbook == null)
+            {
+                MessageBox.Show("Select the Task Workbook first to proceed...");
+            }
+            else
+            {
+                DataSet ds = new DataSet();
+                DataTable workbookTable = new DataTable();
+                OleDbDataAdapter xlAdapter = new OleDbDataAdapter();
+                OleDbConnection conn = new OleDbConnection();
+                conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + masterWorkbook + ";Extended Properties=\"Excel 12.0;HDR=YES;\"";
+
+                OleDbCommand comm = new OleDbCommand();
+                comm.Connection = conn;
+                conn.Open();
+                workbookTable = comm.Connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                comm.CommandText = "SELECT * FROM [" + workbookTable.Rows[0]["TABLE_NAME"].ToString() + "]";
+                xlAdapter.SelectCommand = comm;
+                xlAdapter.Fill(ds);
+                conn.Close();
+                IEnumerable<string> namesCollection = (from row in ds.Tables[0].AsEnumerable()
+                                                       select row[namesIndex - 1].ToString()).ToList().Distinct();
+
+                foreach (string name in namesCollection)
+                {
+                    var table = from row in ds.Tables[0].AsEnumerable()
+                                where row[namesIndex - 1].ToString() == name
+                                select row;
+
+                    workbook = xlApp.Workbooks.Add();
+                    Excel.Worksheet worksheet = workbook.ActiveSheet;               
+                    try
+                    {
+                        workbook.SaveAs(filePath + name + ".xlsx");
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        workbook.Close();
+                        continue;
+                    }
+
+                    worksheet.Name = masterSheetName;
+                    for (int i = 1; i <= masterColCount; i++)
+                    {
+                        worksheet.Cells[1, i] = ds.Tables[0].Columns[i - 1].ColumnName.ToString();
+                    }
+                    workbook.Save();
+                    workbook.Close();
+                    conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + name + ".xlsx;Extended Properties=\"Excel 12.0;HDR=YES;\"";
+                    comm.Connection = conn;
+                    conn.Open();
+                    workbookTable.Clear();
+                    workbookTable = comm.Connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                
+                    foreach (DataRow row in table)
+                    {
+                        comm.CommandText = "INSERT INTO [" + masterSheetName + "$] VALUES(";
+                        for (int i = 0; i < masterColCount; i++)
+                        {
+                            if (i == masterColCount - 1)
+                                comm.CommandText += "'" + row[i].ToString() + "')";
+                            else
+                                comm.CommandText += "'" + row[i].ToString() + "',";
+                        }
+                        comm.ExecuteNonQuery();
+                    }
+                    conn.Close();
+                    xlApp.Quit();
+                }
+                MessageBox.Show(string.Format("The individual excel files have been generated and placed at the path:\n{0}", filePath));
+            }
+        }
+
+        private string Showdialog()
+        {
+            Form indexForm = new Form();
+            indexForm.Size = new System.Drawing.Size(300, 180);
+            indexForm.MinimizeBox = false;
+            indexForm.MaximizeBox = false;
+            indexForm.StartPosition = FormStartPosition.CenterParent;
+            indexForm.FormBorderStyle = FormBorderStyle.FixedSingle;
+            indexForm.Text = "Enter Master Column Index";
+
+            Label label1 = new Label();
+            label1.Text = "Names Column Index starting from 1:";
+            label1.Top = 20;
+            label1.Left = 17;
+            label1.Width = 250;
+            label1.Height = 30;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+
+            TextBox namesIndexText = new TextBox();
+            namesIndexText.Width = 50;
+            namesIndexText.Top = 4;
+            namesIndexText.Left = 183;
+            indexForm.Controls.Add(namesIndexText);
+            label1.Controls.Add(namesIndexText);
+            indexForm.Controls.Add(label1);
+
+            Label label2 = new Label();
+            label2.Text = "FormID Column Index starting from 1:";
+            label2.Top = 70;
+            label2.Left = 17;
+            label2.Width = 250;
+            label2.Height = 30;
+            label2.BorderStyle = BorderStyle.Fixed3D;
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+
+            TextBox formIdIndexText = new TextBox();
+            formIdIndexText.Width = 50;
+            formIdIndexText.Top = 4;
+            formIdIndexText.Left = 183;
+            indexForm.Controls.Add(formIdIndexText);
+            label2.Controls.Add(formIdIndexText);
+            indexForm.Controls.Add(label2);
+
+            Button acceptButton = new Button();
+            acceptButton.Text = "OK";
+            acceptButton.Left = 110;
+            acceptButton.Top = 110;
+            acceptButton.DialogResult = DialogResult.OK;
+            indexForm.Controls.Add(acceptButton);
+            indexForm.AcceptButton = acceptButton;
+           
+            if (indexForm.ShowDialog(this) == DialogResult.OK)
+            {
+                namesIndex = int.Parse(namesIndexText.Text);
+                formIdIndex = int.Parse(formIdIndexText.Text);
+                return namesIndexText.Text;
+            }
+            else return null;
+        }
+    }
+}
